8f772aa11de3b38dcb73eff5f3a1c9ee67e39bfd
JairoLopeznavarretePXL <jairo.lopeznavarrete@student.pxl.be>
2023-11-23 14:43:19 +0100

    Werkpakket2 componenten plaatsen 4

--- vue-project/src/components/PopularProductsComponent.vue
+++ vue-project/src/components/PopularProductsComponent.vue
@@ -53,11 +53,49 @@
 
 <script>
 export default {
-  props: [
+  producten: [
+    {
+      id: 1,
+      titel: 'Ibanez 3500X',
+      omschrijving: 'Dit gitaar is een zekere aanrader voor gevorderde gitaar spelers, Het heeft een hele dunne en soepele nek waarmee je snel en makkelijk kan shredden.',
+      afbeelding: '@/assets/guitar.png',
+      price: '€500',
+      btw: '25%',
+      stock: 12
+    },
+    {
+      id: 2,
+      titel: 'Pacifica Eg350',
+      omschrijving: 'de Pacifica Eg350 is altijd een aanrader voor beginners, dit gitaar is gemaakt met het eikenhout, waardoor de gitaar zo licht als een veertje is. Dit gitaar heeft 26 frets wat meer is dan de gemiddelde.',
+      afbeelding: '@/assets/guitar-2.png',
+      price: '€300',
+      btw: '40%',
+      stock: 20
+    },
+    {
+      id: 2,
+      titel: 'Pacifica Eg350',
+      omschrijving: 'de Pacifica Eg350 is altijd een aanrader voor beginners, dit gitaar is gemaakt met het eikenhout waardoor de gitaar zo licht als een veertje is. Dit gitaar heeft 26 frets wat meer is dan de gemiddelde.',
+      afbeelding: '@/assets/guitar-3.png',
+      price: '€300',
+      btw: '40%',
+      stock: 20
+    },
+    {
+      id: 4,
+      titel: 'Ibanez XRI 1 (gold)',
+      omschrijving: 'De Ibanez XRI 1 (gold) heeft niet alleen de juiste sounds maar ook de looks van een echte rockgitaar. dit gitaar is speciaal gemaakt voor als je in verschillende tunes wilt kunnen spelen zoals drop #C, drop #D, drop #A, de snaren zijn gemaakt van zilver in plaats van metaal en ze zijn dikker aangelegt als de meeste snaren waardoor je ze makkelijk anders kan afstemmen zonder dat ze kapot springen!',
+      afbeelding: '@/assets/guitar-4.png',
+      price: '€4500',
+      btw: '10%',
+      stock: 2
+    }
+  ]
+  /*props: [
       'name',
       'price'
   ],
-  name: "PopularProductsComponent"
+  name: "PopularProductsComponent"*/
 }
 </script>
 
